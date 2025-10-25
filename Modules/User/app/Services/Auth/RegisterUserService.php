2c16770a1f3c9a8af1152de59ce64e4f480700fa
ghazl khalil <ghazlkhalil99@gmail.com>
2025-10-25 15:13:01 +0300

    edit

--- Modules/User/app/Services/Auth/RegisterUserService.php
+++ Modules/User/app/Services/Auth/RegisterUserService.php
@@ -15,14 +15,10 @@ class RegisterUserService
         protected RegisterUserRepository $userRepository,
         protected HomeService $homeService
     ) {}
-
-    // Register a new user
-    public function register(array $data, $host)
+    public function registerCustomer(array $data)
     {
-        $mainDomain = app()->environment('production')
-            ? config('app.main_domain', 'soqsyria.com')
-            : 'market-platform.localhost';
-        return DB::transaction(function () use ($data, $host, $mainDomain) {
+
+        return DB::transaction(function () use ($data) {
 
             // Create user with hashed password
             $user = $this->userRepository->create([
@@ -30,18 +26,39 @@ class RegisterUserService
                 'email' => $data['email'],
                 'role' => $data['role'],
                 'password' => Hash::make($data['password']),
-                'group_id' => \App\Group::getDefaultGroup()?->id,
             ]);
-            if (! $this->homeService->isMainDomain($host, $mainDomain)) {
-                $store = \Modules\Store\Models\Store::currentFromUrl()->first();
 
-                if (! $store) {
-                    abort(404, 'Store not found');
-                }
-                if (! $user->stores()->where('store_id', $store->id)->exists()) {
-                    $user->stores()->attach($store->id, ['is_active' => true]);
-                }
+            $store = current_store();
+
+            if (! $store) {
+                abort(404, 'Store not found');
             }
+            if (! $user->stores()->where('store_id', $store->id)->exists()) {
+                $user->stores()->attach($store->id, ['is_active' => true]);
+            }
+
+            // Log the user in after successful registration
+            Auth::login($user);
+            $user->last_login_at = now();
+            $user->save();
+
+            // Return the created user instance
+            return $user;
+        });
+    }
+    // Register a new user
+    public function register(array $data)
+    {
+
+        return DB::transaction(function () use ($data) {
+
+            // Create user with hashed password
+            $user = $this->userRepository->createOwner([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'role' => $data['role'],
+                'password' => Hash::make($data['password']),
+            ]);
 
             // Log the user in after successful registration
             Auth::login($user);
