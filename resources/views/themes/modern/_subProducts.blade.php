cdc26111b978154adaf933440afd2ad425329158
ghazl khalil <ghazlkhalil99@gmail.com>
2025-11-13 10:36:36 +0300

    عرض المنتجات الفرعية في باقي الثيم

---
+++ resources/views/themes/modern/_subProducts.blade.php
@@ -0,0 +1,24 @@
+@if ($products->count() > 0)
+    @foreach ($products as $product)
+     @php
+        $media = $product->getFirstMedia('product_images');
+  // راوت للمنتج نفسه
+    @endphp
+        <a href="{{ route('product.show', $product->id) }}" class="product-card-v2" style="text-decoration: none">
+            @if ($media)
+                <div class="product-image">
+
+                    <img src="{{ route('product.image', $media->id) }}" alt="{{ $product->name }}">
+
+                </div>
+            @endif
+            <div class="product-info">
+                <h3 class="product-name">{{ $product->name }}</h3>
+            </div>
+        </a>
+    @endforeach
+@else
+    <div class="alert alert-info text-center w-100">
+        {{ __('No products found for this category.') }}
+    </div>
+@endif
